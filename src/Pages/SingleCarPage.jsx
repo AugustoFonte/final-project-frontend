f8a316d73bd3c8c0701ecf6af32dff9095b1f621
unknown <augustodafonte@gmail.com>
2022-12-11 21:23:49 +0000

    some changes on singlecarpage

--- src/Pages/SingleCarPage.jsx
+++ src/Pages/SingleCarPage.jsx
@@ -25,47 +25,52 @@ function SingleCarPage() {
             alt="car image"
           />
         </div>
-        <div><Testdrive/></div>
-      </div>
-      <div className="py-10 grid grind-cols-2 text-lg">
+        <div className="py-10  text-lg">
         <div>
-        <p className="font-bold px-5 text-sky-800"> Model: <span className="text-black font-normal">{result.modelo}</span></p>
-        </div>
-        <div>
-        <p className="font-bold px-5 text-sky-800">Engine: <span className="text-black font-normal">{result.combustivel}</span></p>
-        </div>
-        <div>
-          <p className="font-bold px-5 text-sky-800">Horsepower: <span className="text-black font-normal">{result.potencia}</span></p>
-        </div>
-        <div>
-          <p className="font-bold px-5 text-sky-800">First register: <span className="text-black font-normal">{result.anofabrico} </span></p>
-        </div>
-        <div>
-          <p className="font-bold px-5 text-sky-800">Body Style: <span className="text-black font-normal">{result.tipodechassi}</span></p>
-        </div>
-        <div>
-          <p className="font-bold px-5 text-sky-800">Mileage: <span className="text-black font-normal">{result.numerokm}</span></p>
-        </div>
-        <div>
-          <p className="font-bold px-5 text-sky-800">Ext. Color: <span className="text-black font-normal">{result.corexterior}</span></p>
-        </div>
-        <div>
-          <p className="font-bold px-5 text-sky-800">Transmission: <span className="text-black font-normal">{result.tranmissao}</span></p>
-        </div>
-        <div>
-          <p className="font-bold px-5 text-sky-800">Doors: <span className="text-black font-normal">{result.portas}</span></p>
-        </div>
-        <div>
-          <p className="font-bold px-5 text-sky-800">Seats: <span className="text-black font-normal">{result.capacidade}</span></p>
-        </div>
-        <div>
-          <p className="font-bold px-5 text-sky-800">Sensors: <span className="text-black font-normal">{result.sensores}</span></p>
-        </div>
-        <div>
-          <p className="font-bold px-5 text-sky-800">Lights: <span className="text-black font-normal">{result.tipofarois}</span></p>
-        </div>
+          <p className="text-5xl font-bold py-10 px-5 text-sky-800">Price: {result.preco}€</p>
+          </div>
+          <div>
+          <p className="font-bold px-5 text-sky-800"> Model: <span className="text-black font-normal">{result.modelo}</span></p>
+          </div>
+          <div>
+          <p className="font-bold px-5 text-sky-800">Engine: <span className="text-black font-normal">{result.combustivel}</span></p>
+          </div>
+          <div>
+            <p className="font-bold px-5 text-sky-800">Horsepower: <span className="text-black font-normal">{result.potencia}</span></p>
+          </div>
+          <div>
+            <p className="font-bold px-5 text-sky-800">First register: <span className="text-black font-normal">{result.anofabrico} </span></p>
+          </div>
+          <div>
+            <p className="font-bold px-5 text-sky-800">Body Style: <span className="text-black font-normal">{result.tipodechassi}</span></p>
+          </div>
+          <div>
+            <p className="font-bold px-5 text-sky-800">Mileage: <span className="text-black font-normal">{result.numerokm}</span></p>
+          </div>
+          <div>
+            <p className="font-bold px-5 text-sky-800">Ext. Color: <span className="text-black font-normal">{result.corexterior}</span></p>
+          </div>
+          <div>
+            <p className="font-bold px-5 text-sky-800">Transmission: <span className="text-black font-normal">{result.tranmissao}</span></p>
+          </div>
+          <div>
+            <p className="font-bold px-5 text-sky-800">Doors: <span className="text-black font-normal">{result.portas}</span></p>
+          </div>
+          <div>
+            <p className="font-bold px-5 text-sky-800">Seats: <span className="text-black font-normal">{result.capacidade}</span></p>
+          </div>
+          <div>
+            <p className="font-bold px-5 text-sky-800">Sensors: <span className="text-black font-normal">{result.sensores}</span></p>
+          </div>
+          <div>
+            <p className="font-bold px-5 text-sky-800">Lights: <span className="text-black font-normal">{result.tipofarois}</span></p>
+          </div>
         
       </div>
+       
+      </div>
+      <div className="flex "><Testdrive/></div>
+      
       <div className="py-10"></div>
     </MainLayout>
   );
